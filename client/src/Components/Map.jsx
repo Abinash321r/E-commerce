a7ad3c0341613242ac0224594628d37cb51022ff
Abinash321r <abinashnepali619@gmail.com>
2025-09-17 17:15:38 +0545

    updated map

--- client/src/Components/Map.jsx
+++ client/src/Components/Map.jsx
@@ -10,7 +10,7 @@ function Map() {
         lng: 85.2807,
       };
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_URL}>
+    <LoadScript >
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
       zoom={15}
