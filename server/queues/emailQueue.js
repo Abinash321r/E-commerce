aff58e44a04a19a0364939b241803b6434a06d22
Abinash321r <abinashnepali619@gmail.com>
2026-02-25 21:50:25 +0545

    updated redis queue

--- server/queues/emailQueue.js
+++ server/queues/emailQueue.js
@@ -1,25 +1,40 @@
 import Bull from "bull";
 import { transporter } from "../Middlewares/Transporter.js";
-import 'dotenv/config';
+import "dotenv/config";
 
+console.log("Email queue worker starting...");
 
-const redisConfig = process.env.REDIS_URL
-  ? process.env.REDIS_URL
-  : {
+let emailQueue;
+
+if (process.env.REDIS_URL) {
+  emailQueue = new Bull("email-queue", {
+    redis: {
+      url: process.env.REDIS_URL
+    },
+  });
+} else {
+  emailQueue = new Bull("email-queue", {
+    redis: {
       host: process.env.REDIS_HOST || "127.0.0.1",
       port: process.env.REDIS_PORT || 6379,
-    };
+    },
+  });
+}
 
-const emailQueue = new Bull("email-queue", redisConfig);
-/*const emailQueue = new Bull("email-queue", {
-  redis: { host:`${process.env.REDIS_HOST}`, port: `${process.env.REDIS_PORT}` },
-});*/
+emailQueue.client
+  .ping()
+  .then((res) => console.log(" Redis connected:", res))
+  .catch((err) => console.error("Redis connection failed:", err));
 
-// Job Processor
 emailQueue.process(async (job) => {
+  console.log(" Processing job:", job.id);
   const { mailOptions } = job.data;
   await transporter.sendMail(mailOptions);
-  console.log("Email sent successfully!");
+  console.log(" Email sent successfully!");
+});
+
+emailQueue.on("error", (err) => {
+  console.error("Queue error:", err);
 });
 
 export default emailQueue;
