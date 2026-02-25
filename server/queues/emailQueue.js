91cb35ef7564d49eeae9cdbe59be4c86cd62e557
Abinash321r <abinashnepali619@gmail.com>
2026-02-25 16:05:34 +0545

    updated emailQueue

--- server/queues/emailQueue.js
+++ server/queues/emailQueue.js
@@ -2,9 +2,18 @@ import Bull from "bull";
 import { transporter } from "../Middlewares/Transporter.js";
 import 'dotenv/config';
 
-const emailQueue = new Bull("email-queue", {
+
+const redisConfig = process.env.REDIS_URL
+  ? process.env.REDIS_URL
+  : {
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: process.env.REDIS_PORT || 6379,
+    };
+
+const emailQueue = new Bull("email-queue", redisConfig);
+/*const emailQueue = new Bull("email-queue", {
   redis: { host:`${process.env.REDIS_HOST}`, port: `${process.env.REDIS_PORT}` },
-});
+});*/
 
 // Job Processor
 emailQueue.process(async (job) => {
