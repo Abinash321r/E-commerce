15656f34f07490f21d078b9551445001c6f9fc6c
Abinash321r <abinashnepali619@gmail.com>
2026-02-25 23:37:24 +0545

    updated transporter

--- server/Middlewares/Transporter.js
+++ server/Middlewares/Transporter.js
@@ -2,11 +2,11 @@
  import 'dotenv/config';
 
  export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for 587
+  service: "gmail",
   auth: {
-    user: `${process.env.TRANSPORTER_EMAIL}`,   // your Gmail
-    pass: `${process.env.TRANSPORTER_PASSWORD}`,      // the app password you generated
+    user: process.env.TRANSPORTER_EMAIL,
+    pass: process.env.TRANSPORTER_PASSWORD,
   },
 });
+
+Password
