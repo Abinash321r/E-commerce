d24a9dbfd1f1649c683c8d6820a67d01e51c1958
Abinash321r <abinashnepali619@gmail.com>
2026-02-26 00:08:30 +0545

    updated transporter last

--- server/Middlewares/Transporter.js
+++ server/Middlewares/Transporter.js
@@ -1,10 +1,15 @@
  import nodemailer from 'nodemailer';
  import 'dotenv/config';
 
- export const transporter = nodemailer.createTransport({
-  service: "gmail",
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false for 587
   auth: {
     user: process.env.TRANSPORTER_EMAIL,
     pass: process.env.TRANSPORTER_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
