c440d90b91a951fce7fa92d899e6049333c03930
Abinash321r <abinashnepali619@gmail.com>
2026-02-26 16:02:42 +0545

    updated transporteer again

--- server/Middlewares/Transporter.js
+++ server/Middlewares/Transporter.js
@@ -1,10 +1,15 @@
- import nodemailer from 'nodemailer';
- import 'dotenv/config';
+import nodemailer from "nodemailer";
+import "dotenv/config";
 
 export const transporter = nodemailer.createTransport({
- service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS upgrade
   auth: {
     user: process.env.TRANSPORTER_EMAIL,
-    pass: process.env.TRANSPORTER_PASSWORD, // Gmail App Password
+    pass: process.env.TRANSPORTER_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
